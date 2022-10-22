498036d5b3925fa029459763990902637d23c835
kingar7 <kingar7@ceclnx01-19.csi.miamioh.edu>
2022-10-22 13:59:53 -0400

    Submission

--- challenging.pl
+++ challenging.pl
@@ -3,13 +3,104 @@
 
 url("TODO: Replace this with the URL of the puzzle").
 
-solution([["TODO: Replace this with the correct solution to the puzzle!"]]).
+solution([ [41,	'Cory',		'Archery',	    	'John Travolta'	],
+  [51,	'Anna',	    'Reading',			'Russell Crowe'	],
+  [52,	'Teagan',	'Hang gliding',	    'Bruce Willis' 	],
+  [73,	'Adrian',	'Music',			'Jude Law'  	], 
+  [74,  'Maria',  	'Writing',			'Tom Cruise'	]
+  ]).
+
 
 solve(T) :-
-	% TODO: Implement this to solve the puzzle. 
-	true.
+   T =[[41,	N1,	H1,	A1],
+      [51,	N2,	H2,	A2],
+      [52,	N3,	H3,	A3],
+      [73,	N4,	H4,	A4],
+      [74, 	N5,	H5,	A5]
+      ],
+    
+   permutation([H1, H2, H3, H4, H5], 
+               ['Archery', 'Reading', 'Hang gliding', 'Music', 'Writing']),
+   clue8(T), % _, H, _
+   clue13(T), % _, H, _
+    
+   permutation([N1, N2, N3, N4, N5], 
+               ['Cory',	'Anna',	'Teagan', 'Adrian', 'Maria' ]),
+   clue2(T), % N, _, _
+   clue5(T), % N, H, _
+   clue9(T), % N, H, _
+   clue10(T), % N, H, _
+   clue12(T), % N, H, _
+  
+   clue10(T),
+   permutation([A1, A2, A3, A4, A5], 
+               ['John Travolta', 'Russell Crowe', 'Bruce Willis', 'Jude Law', 'Tom Cruise']),
+   clue1(T), % _, H, A
+   clue3(T), % N, H, A
+   clue4(T), % _, H, A
+   clue6(T), % N, _, A
+   clue7(T), % _, _, A
+   clue11(T). % N, _, A
 
 
+	% 1. Tom Cruise's cousin enjoys writing.
+clue1(T) :- member([_, _, 'Writing', 'Tom Cruise'], T).
+
+% 2. The collector who has 74 baseball cards is Maria.
+clue2(T) :- member([74, 'Maria', _, _], T).
+
+% 3. The 5 people were the person who enjoys writing, Cory, 
+% Jude Law's cousin, the collector who has 51 baseball cards, 
+% and the person who enjoys hang gliding
+clue3(T) :- member([Num, N, H, 'Jude Law'], T), 
+    Num \= 51, N \= 'Cory', H \= 'Hang gliding', H \= 'Writing',
+    member([Num1, N1, 'Writing', A1], T),
+    Num1 \= 51, N1 \= 'Cory', A1 \= 'Jude Law',
+    member([Num2, 'Cory', H2, A2], T), 
+    Num2 \= 51, H2 \= 'Writing', H2 \= 'Hang gliding', A2 \= 'Jude Law',
+    member([51, N3, H3, A3], T), 
+    N3 \= 'Cory', H3 \= 'Hang gliding', H3 \= 'Writing', A3 \= 'Jude Law',
+    member([Num4, N4, 'Hang gliding', A4], T),
+    Num4 \= 51, N4 \= 'Cory', A4 \= 'Jude Law'.
+
+% 4. Russell Crowe's cousin does not enjoy hang gliding.
+clue4(T) :- member([_, _, H, 'Russell Crowe'], T), H \= 'Hang gliding'.
+
+
+% 5. Adrian has more baseball cards than the person who enjoys reading.
+clue5(T) :- member([Num1, 'Adrian', _, _], T), 
+    member([Num2, _, 'Reading', _], T),
+    Num1 > Num2.
+
+% 6. John Travolta's cousin is Cory.
+clue6(T) :- member([_, 'Cory', _, 'John Travolta'], T).
+
+
+% 7. The collector who has 41 baseball cards isn't related to Bruce Willis.
+clue7(T) :- member([41, _, _, A], T), A \= 'Bruce Willis'.
+
+% 8. The person who enjoys archery has fewer baseball cards than the person
+%  who enjoys reading.
+clue8(T) :- member([Num1, _, 'Archery', _], T),
+    member([Num2, _, 'Reading', _], T),
+    Num1 < Num2.
+
+% 9. The person who enjoys reading is not Cory.
+clue9(T) :- member([_, N, 'Reading', _], T), N \= 'Cory'.
+
+% 10. Maria has more baseball cards than the person who enjoys hang gliding.
+clue10(T) :-  member([Num1, 'Maria', _, _], T), 
+     member([Num2, _, 'Hang gliding', _], T),
+    Num1 > Num2.
+
+% 11. Russell Crowe's cousin is not Teagan.
+clue11(T) :- member([_, N, _, 'Russell Crowe'], T), N \= 'Teagan'.
+
+% 12. The person who enjoys music is Adrian.
+clue12(T) :- member([_, 'Adrian', 'Music', _], T).
+
+% 13. The collector who has 52 baseball cards does not enjoy music.
+clue13(T) :- member([52, _, H, _], T), H \= 'Music'.
 check :- 
 	% Confirm that the correct solution is found
 	solution(S), (solve(S); not(solve(S)), writeln("Fails Part 1: Does  not eliminate the correct solution"), fail),
--- veryhard.pl
+++ veryhard.pl
@@ -3,11 +3,141 @@
 
 url("TODO: Replace this with the URL of the puzzle").
 
-solution([["TODO: Replace this with the correct solution to the puzzle!"]]).
+solution([ [5.75,	'Basketball',		'Johnnie',	    	'Perris'		],
+  [6.75,	'Fruit basket',	    'Everett',			'Quimby'		],
+  [7.75,	'Computer',			'Pablo',	    	'Fontanelle'	],
+  [8.75,	'Rare book',		'Luther',			'Oakdale'  		], 
+  [9.75,  	'Toaster',  		'Willie',			'Newport'		],
+  [10.75,  	'Tea set',  		'Brad',				'Mattawamkeag'	]
+  ]).
 
 solve(T) :-
-	% TODO: Implement this to solve the puzzle. 
-	true.
+	T =[[5.75,	I1,	C1,	T1],
+      [6.75,	I2,	C2,	T2],
+      [7.75,	I3,	C3,	T3],
+      [8.75,	I4,	C4,	T4],
+      [9.75, 	I5,	C5,	T5],
+      [10.75, 	I6,	C6,	T6]
+      ],
+    
+   permutation([I1, I2, I3, I4, I5, I6], 
+               ['Basketball', 'Fruit basket', 'Computer', 'Rare book', 'Toaster', 'Tea set']),
+   
+    
+   permutation([C1, C2, C3, C4, C5, C6], 
+               ['Johnnie',	'Everett',	'Pablo', 'Luther', 'Willie', 'Brad']),
+   
+   
+   clue4(T), % I, C, _
+   clue9(T), % I, C, _
+   clue10(T), % _, C, _
+   clue11(T), % _, C, _
+   clue12(T), % I, C, _
+  
+ 
+   permutation([T1, T2, T3, T4, T5, T6], 
+               ['Perris', 'Quimby', 'Fontanelle', 'Oakdale', 'Newport', 'Mattawamkeag']),
+   clue1(T), % I, _, T
+   clue2(T), % _, C, T
+   clue3(T), % I, C, T
+   clue5(T), % I, _, T
+   clue6(T), % I, _, T
+   clue7(T), % _, C, T
+   clue8(T), % I, C, T
+   clue13(T). % _, C, T
+  
+% 1. The package with the rare book in it cost 2 dollars less
+%  than the package going to Mattawamkeag.
+clue1(T) :- member([P1, 'Rare book', _, T1], T),
+    member([P2, I1, _, 'Mattawamkeag'], T),
+    T1 \= 'Mattawamkeag', I1 \= 'Rare book',
+    (P1 is P2 - 2.00).
+    
+
+% 2. The shipment going to Quimby cost somewhat less than Pablo's package.
+clue2(T) :- member([P1, _, _, 'Quimby'], T),
+    member([P2, _, 'Pablo', _], T),
+    P1 < P2.
+
+% 3. The six packages are Luther's package, the shipment 
+% going to Mattawamkeag, the package going to Newport Beach, 
+% the package with the basketball in it, the package with the 
+% fruit basket in it and the package going to Fontanelle.
+
+clue3(T) :- member([_, I, 'Luther', T0], T), 
+    I \= 'Basketball', I \= 'Fruit basket', 
+    T0 \= 'Mattawamkeag', T0 \= 'Newport Beach', T0 \= 'Fontanelle',
+    
+    member([_, I1, C1, 'Mattawamkeag'], T),
+    I1 \= 'Basketball', I1 \= 'Fruit basket', C1 \= 'Luther',
+    
+    member([_, I2, C2, 'Newport'], T), 
+    I2 \= 'Basketball', I2 \= 'Fruit basket', C2 \= 'Luther',
+    
+    member([_, 'Basketball', C3, T3], T), 
+    C3 \= 'Luther', T3 \= 'Mattawamkeag', 
+    T3 \= 'Newport Beach', T3 \= 'Fontanelle',
+    
+    member([_, 'Fruit basket', C4, T4], T),
+    C4 \= 'Luther', T4 \= 'Mattawamkeag', 
+    T4 \= 'Newport Beach', T4 \= 'Fontanelle',
+    
+    member([_, I5, C5, 'Fontanelle'], T),
+    I5 \= 'Basketball', I5 \= 'Fruit basket', C5 \= 'Luther'.
+
+% 4. The package with the tea set in it isn't Pablo's.
+clue4(T) :- member([_, 'Tea set', C, _], T), C \= 'Pablo'.
+
+
+% 5. The shipment going to Fontanelle is either the shipment with the
+%  toaster in it or the package that cost $7.75.
+clue5(T) :-  member([P, I, _, 'Fontanelle'], T), 
+   (P = 7.75, I \= 'Toaster' ; I = 'Toaster', P \= 7.75).
+
+% 6. The package with the basketball in it cost
+%  2 dollars less than the shipment going to Fontanelle.
+clue6(T) :- member([P1, 'Basketball', _, _], T),
+    member([P2, _, _, 'Fontanelle'], T),
+    (P1 is P2 - 2.0).
+
+
+% 7. Of the shipment going to Perris and the shipment going to Mattawamkeag,
+%  one cost $5.75 and the other is Brad's.
+clue7(T) :- member([P1, _, C1, 'Mattawamkeag'], T),
+    member([P2, _, C2, 'Perris'], T),
+    (P1 = 5.75, C1 \= 'Brad', P2 \= 5.75, C2 = 'Brad' ; 
+	P1 \= 5.75, C1 = 'Brad', P2 = 5.75, C2 \= 'Brad').
+
+% 8. Of the package going to Mattawamkeag and the shipment that cost $6.75,
+%  one is Brad's and the other contains the fruit basket.
+clue8(T) :- member([P1, I1, C1, 'Mattawamkeag'], T),
+    member([6.75, I2, C2, T2], T),
+    (I1 = 'Fruit basket', C1 \= 'Brad', I2 \= 'Fruit basket', C2 = 'Brad' ;
+     I1 \= 'Fruit basket', C1 = 'Brad', I2 = 'Fruit basket', C2 \= 'Brad'),
+    P1 \= 6.75, T2 \= 'Mattawamkeag'.
+
+% 9. Everett's package is either the shipment with the toaster in it
+%  or the package that cost $6.75.
+clue9(T) :- member([P, I, 'Everett', _], T), 
+     (P = 6.75, I \= 'Toaster' ; I = 'Toaster', P \= 6.75).
+
+% 10. The shipment that cost $9.75 isn't Everett's.
+clue10(T) :-  member([9.75, _, C, _], T), C \= 'Everett'. 
+
+
+% 11. The shipment that cost $7.75 isn't Willie's.
+clue11(T) :- member([7.75, _, C, _], T), C \= 'Willie'.
+
+% 12. Brad's shipment is either the package with the toaster in it
+%  or the shipment that cost $10.75.
+clue12(T) :- member([P, I, 'Brad', _], T),
+ (P = 10.75, I \= 'Toaster' ; I = 'Toaster', P \= 10.75).
+
+% 13. Johnnie's package cost 4 dollars less than
+%  the shipment going to Newport Beach.
+clue13(T) :- member([P1, _, 'Johnnie', _], T), 
+    member([P2, _, _, 'Newport'], T),
+    (P1 is P2 - 4.00).
 
 
 
